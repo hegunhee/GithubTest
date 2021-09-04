cf6a2fe4c3b646a388afb88ccd1999bd0420444b
gunhe <hegunhee>
2021-09-04 23:08:31 +0900

    hello Github

--- app/src/main/java/com/hegunhee/githubtest/MainActivity.kt
+++ app/src/main/java/com/hegunhee/githubtest/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.hegunhee.githubtest
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import com.hegunhee.githubtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+
+
+    }
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initViews() = with(binding){
+        textView.setOnTouchListener { _, motionEvent ->
+            when(motionEvent.action){
+                MotionEvent.ACTION_DOWN -> textView.text = "Hello AndroidWorld"
+                MotionEvent.ACTION_UP -> textView.text = "Hello GithubWorld"
+            }
+
+            return@setOnTouchListener true
+        }
+
+
     }
 }
